145111af76b44d7dc7d5022587a75b3633bbb8b9
nayenjoaquin <jnayen2017@udec.cl>
2022-10-05 17:55:36 -0300

    script update and results

--- go.sh
+++ go.sh
@@ -1,13 +1,13 @@
 FECHA=$(date +"%Y-%m-%d-%H-%M-%S")
 
 
-for i in 001 .. 010 .. 001
+for i in 001 002 003 004 005 006 007 008 009 010
 do
-    e1=$(python3 grasp.py -i 100-300-$i.txt -th 0.75 -d 0.9 -tL 90 -lsL 5 -tunning 1)
+    e1=`python3 grasp.py -i 100-300-$i.txt -th 0.75 -d 0.9 -tL 90 -lsL 5 -tunning 1`
 
-    e2=$(python3 grasp.py -i 100-300-$i.txt -th 0.80 -d 0.9 -tL 90 -lsL 5 -tunning 1)
+    e2=`python3 grasp.py -i 100-300-$i.txt -th 0.80 -d 0.9 -tL 90 -lsL 5 -tunning 1`
 
-    e3=$(python3 grasp.py -i 100-300-$i.txt -th 0.85 -d 0.85 -tL 90 -lsL 5 -tunning 1)
+    e3=`python3 grasp.py -i 100-300-$i.txt -th 0.85 -d 0.85 -tL 90 -lsL 5 -tunning 1`
 
     linea="$i; $e1; $e2; $e3"
     echo $linea >> results/resultados-$FECHA.csv
